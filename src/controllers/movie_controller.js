05c0dc1d05da44687b5bc2cc9658aa3029cccfa2
Felipe Matheus Costa <ld17038@gmail.com>
2021-08-27 23:46:14 -0300

    Verificação da existência filme

--- src/controllers/movie_controller.js
+++ src/controllers/movie_controller.js
@@ -12,13 +12,18 @@ router.post('/cadastro', async(req,res)=>{
                 estoque: req.body.estoque
             })
             //salva a variavel no banco dedados
+            if(await Filme.findOne({titulo: data.titulo})){
+                res.send('Erro: filme já existe')
+            }else{
+                //salva a variavel no banco dedados
             data.save().then(()=>{
-               console.log( 'O filme foi cadastrado')
-               res.send({data})
-            }).catch((err)=>{
-                //caso ocorra um erro ao salvar: futuramente servirá para testar se o filme adicionado ja existe
-               res.send(`Erro: Filme já existe`)
-            })   
+                console.log( 'O filme foi cadastrado')
+                res.send({data})
+             }).catch((err)=>{
+                 //caso ocorra um erro ao salvar: futuramente servirá para testar se o filme adicionado ja existe
+                res.send(`Erro: Filme já existe`)
+             }) 
+            }
             //tratamento de erros no cadastro    
     } catch(error) {
         res.status(400).send({erro: `Falha ao cadastrar o filme`})
