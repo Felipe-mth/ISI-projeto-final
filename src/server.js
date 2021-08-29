35163f97de33932527ad62bda73a75edda51f936
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-08-29 12:35:22 -0300

    cors configuration

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 const porta = 8080;
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use((req,res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+    res.setHeader('Access-Control-Allow-Headers',"*");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+
 //Chama o controller de filmes passando app para ele
 require('./controllers/movie_controller')(app);
 //página inicial
