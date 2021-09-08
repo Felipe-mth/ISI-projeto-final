a50dcfb491c90fd7aa31545db492478f664781f3
Felipe Matheus Costa <86928483+Felipe-mth@users.noreply.github.com>
2021-09-08 20:09:23 -0300

    Update series_controller.js

--- src/controllers/series_controller.js
+++ src/controllers/series_controller.js
@@ -3,54 +3,62 @@ const Serie = require('../models/Series');
 const router = express.Router();
 
 router.post('/cadastro', async(req,res)=>{
+    
     try {  
             //salva o que vem do front dentro da variavel data              
             const data = await new Serie({
                 titulo: req.body.titulo,
                 nota: req.body.nota,
                 sinopse: req.body.sinopse,
-                estoque: req.body.estoque
+                estoque: req.body.estoque,
+                temporadas:  req.body.temporadas
             })
             //salva a variavel no banco de dados
-            if(await Serie.findOne({titulo: data.titulo})){
-                res.send('Erro: Serie já existe')
+            titulo = await Filme.findOne({titulo: req.body.titulo});
+            if(titulo){
+                res.status(400).send('Erro: Serie já existe')
             }else{
                 //salva a variavel no banco dedados
             data.save().then(()=>{
                 console.log( 'A serie foi cadastrado')
-                res.send({data})
+                res.status(200).send({data})
              }).catch((err)=>{
-                res.send(`Erro: Não foi possível salvar a serie`)
+                res.status(400).send(`Erro: Não foi possível salvar a serie`)
              }) 
             }
             //tratamento de erros no cadastro    
     } catch(error) {
-        res.status(400).send({erro: `Falha ao cadastrar a serie`})
+        res.status(500).send({erro: `Falha ao cadastrar a serie`})
     }
 });
 
 router.get('/lista', (req,res)=>{
-    Serie.find().lean().then((series)=>{
-        res.send(series);
-    })
-    
-})
+    try {
+        Serie.find().lean().then((serie)=>{
+            res.status(200).send(serie);
+        }).catch((err)=>{
+            res.status(404).send('Serie não encontrados')
+        })
+    } catch (error) {
+        res.status(500).send(`Ocorreu o erro: ${error}`)
+    }   
+});
 
 router.post('/editar/:titulo', async(req,res)=>{
     //busca uma Serie com o mesmo titulo do que vem no url e salva em data
     data = await Serie.findOne({titulo: req.params.titulo});
     //se tiver um serie salvo em data ele edita
     if(data){
-        Serie.findOneAndUpdate({titulo: req.params.titulo}, {titulo: req.body.titulo, nota:req.body.nota, sinopse:req.body.sinopse, estoque: req.body.estoque, data: Date.now}).then(()=>{
-            res.send('show')
+        Serie.findOneAndUpdate({titulo: req.params.titulo}, {titulo: req.body.titulo, nota:req.body.nota, sinopse:req.body.sinopse, estoque: req.body.estoque, temporadas:  req.body.temporadas, data: Date.now}).then(()=>{
+            res.status(200).send('serie atualizado')
         }).catch((err)=>{
-            res.send(err)
+            res.status(500).send(err)
         })
         //caso contrário o serie digitado não existe
     }else{
-        res.send('Serie não existe')
+        res.status(404).send('Serie não existe')
     }
-})
+});
 
 router.post('/deletar/:titulo', async(req,res)=>{
     //busca uma serie com o mesmo titulo do que vem no url e salva em data
@@ -58,15 +66,15 @@ router.post('/deletar/:titulo', async(req,res)=>{
     //se data tem uma serie essa serie e deletado
     if(data){
         Serie.findOneAndDelete({titulo: req.params.titulo}).then(()=>{
-            res.send('Deletada com sucesso')
+            res.status(200).send('Serie deletada com sucesso')
         }).catch((err)=>{
-            res.send(err)
+            res.status(500).send(err)
         })
         //caso contrario o filme não existe
     }else{
-        res.send('Serie não existe')
+        res.status(404).send('Serie não existe')
     }
     
-})
+});
 
 module.exports = app=> app.use('/serie', router);
